a76f83f6c44bd4d4f2c859e3d02661880e290050
Bruhbro22 <donmadhu411@gmail.com>
2026-06-01 10:24:33 -0500

    Add files via upload

---
+++ coolstuff1.py
@@ -0,0 +1,27 @@
+#BETA VERSION OF AI ANISH CHAT BOT MADE BY ANISH DONTHULA
+import keyword
+
+print ("Hello player, what will be your username?")
+
+name=input 
+
+print("Hello",name,"welcome to this world!")
+print("What are your favorite hobbies?")
+
+hobby=input
+
+print("Nice,",hobby,"sounds cool!")
+print("I want all the keywords if the answer is 9 so this answer will be...",3*3,".")
+
+print(keyword.kwlist)
+print("Sorry, what is your name again?")
+
+name=input
+
+print("Sorry for forgetting",name,"I will never forget!")
+print("How are you feeling today,",name)
+
+feeling=input
+
+print("I am sorry for your loss,",feeling)
+print("ERroR........10101110101011001010110101001111000101010101010001110010101010011010")
